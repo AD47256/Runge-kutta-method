b6d81d390c2dff86456b77975cc469fe7ac1d6a8
w <2804550199@qq.com>
2022-12-01 17:44:34 +0800

    Initial Commit

---
+++ Runge kutta method/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Runge kutta method
+//
+//  Created by WC on 2022/12/1.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
